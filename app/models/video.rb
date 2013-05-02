d8d92f3917e11325ae656812bcc594f3b5437337
Numan Ilyas <numan.ilyas@gmail.com>
2013-05-02 13:43:29 -0400

    message

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,13 +3,11 @@ class Video < ActiveRecord::Base
   
   belongs_to :video_category
   
-  VALID_URL_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  VALID_URL_REGEX = /^https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube\.com\S*[^\w\-\s])([\w\-]{11})(?=[^\w\-]|$)(?![?=&+%\w]*(?:['"][^<>]*>|<\/a>))[?=&+%\w-]*/ix
   
   validates :video_category_id, presence: true
   validates :title, presence: true, length: {maximum: 50}, uniqueness: true
-  validates :url, presence: true, length: {maximum: 50}, format: { with: VALID_URL_REGEX }
-  
-  
+  validates :url, presence: true, length: {maximum: 50}, format: { with: VALID_URL_REGEX } 
     
   
 end
